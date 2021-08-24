8501c8741c7a8bb977ccad8b8a8df37d82d7b42f
bigcatplichta <bigcatplichta@gmail.com>
2021-08-24 13:43:02 -0600

    add router and switches

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router, Route, Switch, useParams } from 'react-router-dom';
+import CardExampleCard from './WishCard';
+import Home from './pages/Home'
+
+const API_URL = 'http://localhost:8000'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +24,17 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+    <Router>
+      {/* <NavBar /> */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/animals" component={CardExampleCard}/>
+        {/* <Route path="/animals/:id" component={AnimalDetail} />
+        <Route exact path="/donations" component={DonationIndex} />
+        <Route path="/wishes/:id" component={WishDetail} /> */}
+      </Switch>
+    </Router>
     </div>
   );
 }
