ef7c5f26d1032745ed3a2ddd825dead587df270a
bigcatplichta <bigcatplichta@gmail.com>
2021-08-24 13:43:24 -0600

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import Landing from './Landing';
+// import AnimalSelectTabs from './AnimalSelectTabs'
+
+
+// Render Landing only if there is no current session and unmount as soon as it is no longer in view
+// How to do that?
+
+export default function Home() {
+
+    return (
+        <>
+        {/* <Landing />
+        <AnimalSelectTabs /> */}
+        <h1>This is the home page</h1>
+        </>
+    )
+}
