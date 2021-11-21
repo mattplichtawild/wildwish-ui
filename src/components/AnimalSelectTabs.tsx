b42882fa78f355dbce3359994504f18d62e5c166
bigcatplichta <bigcatplichta@gmail.com>
2021-11-21 16:38:16 -0700

    remove param from fetchData

--- src/components/AnimalSelectTabs.tsx
+++ src/components/AnimalSelectTabs.tsx
@@ -1,10 +1,11 @@
-
+import WishCarousel from "./WishCarousel"
 
 export default function AnimalSelectTabs() {
 
     return (
         <div>
             <h1>Animals Index</h1>
+            <WishCarousel url='featured'/>
         </div>
     )
 }
