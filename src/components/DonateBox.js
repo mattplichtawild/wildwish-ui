0bb5600431de38096401adf9c4698d3415c48062
bigcatplichta <bigcatplichta@gmail.com>
2021-12-12 12:16:32 -0700

    add input elements

--- src/components/DonateBox.js
+++ src/components/DonateBox.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import DonateModal from './DonateModal' 
-// import clsx from 'clsx';
-import { Input, Container, IconButton, Typography} from '@material-ui/core'
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove'
+import { Button, Input } from 'semantic-ui-react'
 
-export default function DonateBox(props) {
+export default function DonateBox() {
 
     const [amount, setAmount] = React.useState(1)
     const handleChange = (event) => {
@@ -24,32 +20,82 @@ export default function DonateBox(props) {
 
     return (
         <>
-        <Typography variant='overline'>Help their wish come true</Typography>
-        <Container name='Amount Pick'>
-            <IconButton onClick={decreaseAmount} size='medium' >
-                <RemoveIcon fontSize='inherit' color='inherit' />
-            </IconButton>
-            <Typography variant='overline'>$</Typography>
-            <Input 
-                id='amount'
-                name='amount'
-                color='primary'
-                // disabled={!hasActiveWish}
-                disableUnderline
-                type='number'
-                inputProps={{min: 1}}
-                onChange={handleChange}
-                value={amount}
-                // inputComponent='number'
-            />
-            <IconButton onClick={increaseAmount} size='medium' >
-                <AddIcon fontSize='inherit' color='inherit'/>
-            </IconButton>
-            {/* <label htmlFor="amount"/>
-            <input type='number' name='amount' id='amount' value={amount} min='1' onChange={handleChange}/> */}
-            
-            <DonateModal amount={amount} data={props.data} />
-        </Container>
+        <a>Donate Box</a>
+        <Button icon='plus' onClick={increaseAmount}/>
+        <Input 
+            id='amount'
+            name='amount'
+            color='primary'
+            // disabled={!hasActiveWish}
+            disableUnderline
+            type='number'
+            inputProps={{min: 1}}
+            onChange={handleChange}
+            value={amount}
+            // inputComponent='number'
+        />
+        <Button icon='minus' onClick={decreaseAmount}/>
         </>
     )
 }
+
+
+
+
+
+
+// import React from 'react'
+// import DonateModal from './DonateModal' 
+// // import clsx from 'clsx';
+// import { Input, Container, IconButton, Typography} from '@material-ui/core'
+// import AddIcon from '@material-ui/icons/Add';
+// import RemoveIcon from '@material-ui/icons/Remove'
+
+// export default function DonateBox(props) {
+
+//     const [amount, setAmount] = React.useState(1)
+//     const handleChange = (event) => {
+//         setAmount(event.target.value)
+//     }
+
+//     const increaseAmount = () => {
+//         setAmount(parseInt(amount) + 1)
+//     }
+
+//     const decreaseAmount = () => {
+//         if (amount > 1) {
+//             setAmount(parseInt(amount) - 1)
+//         }
+//     }
+
+//     return (
+//         <>
+//         <Typography variant='overline'>Help their wish come true</Typography>
+//         <Container name='Amount Pick'>
+//             <IconButton onClick={decreaseAmount} size='medium' >
+//                 <RemoveIcon fontSize='inherit' color='inherit' />
+//             </IconButton>
+//             <Typography variant='overline'>$</Typography>
+//             <Input 
+//                 id='amount'
+//                 name='amount'
+//                 color='primary'
+//                 // disabled={!hasActiveWish}
+//                 disableUnderline
+//                 type='number'
+//                 inputProps={{min: 1}}
+//                 onChange={handleChange}
+//                 value={amount}
+//                 // inputComponent='number'
+//             />
+//             <IconButton onClick={increaseAmount} size='medium' >
+//                 <AddIcon fontSize='inherit' color='inherit'/>
+//             </IconButton>
+//             {/* <label htmlFor="amount"/>
+//             <input type='number' name='amount' id='amount' value={amount} min='1' onChange={handleChange}/> */}
+            
+//             <DonateModal amount={amount} data={props.data} />
+//         </Container>
+//         </>
+//     )
+// }
